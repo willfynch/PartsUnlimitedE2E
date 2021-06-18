b7b9286956c3a120bab29dc4f0baae72fe6f1b3c
willfynch <william.finzy@protonmail.com>
2021-06-18 11:57:05 +0200

    Added comments

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-        // My first change again 
+        // My second change now !!! 
         
         [Key]
         public int CartItemId { get; set; }
